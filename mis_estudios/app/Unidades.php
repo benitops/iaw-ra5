82cc81ba9b1361ff3b3b5591ae0521f60ee0a85a
benitops <benitoparsan@gmail.com>
2022-02-01 17:52:07 +0100

    __construct() y crearUnidad()

--- mis_estudios/app/Unidades.php
+++ mis_estudios/app/Unidades.php
@@ -2,9 +2,36 @@
 
 class Unidades
 {
-    public int $clave;
-    public int $asignatura; // Asignatura->codigo;
-    public int $numero;
-    public string $nombre;
-    public int $porcentaje;
+    public int      $clave;         //Auto-increment
+    public int      $asignatura;    // Asignatura->codigo;
+    public int      $numero;
+    public string   $nombre;
+    public int      $porcentaje;
+
+    public function __construct($clave = NULL){
+        $this->clave = $clave;
+    }
+
+    public function crearUnidad($asignatura, $numero, $nombre, $porcentaje){
+        $this->asignatura   = $asignatura;
+        $this->numero       = $numero;
+        $this->nombre       = $nombre;
+        $this->porcentaje   = $porcentaje;
+
+        global $db;
+        $query = "INSERT INTO mis_estudios.unidades (asignatura, numero, nombre, porcentaje) VALUES (:asignatura, :numero, :nombre, :porcentaje);";
+        $consulta = $db->prepare($query);
+        $consulta->bindParam(':asignatura', $this->asignatura);
+        $consulta->bindParam(':numero', $this->numero);
+        $consulta->bindParam(':nombre', $this->nombre);
+        $consulta->bindParam(':porcentaje', $this->porcentaje);
+
+        if($consulta->execute()){
+            // Si se ejecuta correctamente, devuelve TRUE
+            return true;
+        } else {
+            // Si no se ejecuta correctamente, devuelve FALSE
+            return false;
+        }
+    }
 }
