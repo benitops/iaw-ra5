7bf1a375977935cec0f9fb80eeab5efd7b0dfb88
benitops <benitoparsan@gmail.com>
2022-02-02 09:58:00 +0100

    Instrumentos.php
    Clase renombrada

---
+++ mis_estudios/app/Instrumentos.php
@@ -0,0 +1,12 @@
+<?php
+
+class Instrumentos
+{
+    public int      $clave;     // Auto-increment
+    public int      $unidad;    // Unidades->clave;
+    public string   $nombre;
+    public int      $peso;
+    public float    $calificacion;
+
+
+}
