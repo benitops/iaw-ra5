dddadefbbe64e4253333f644f94641c966541acf
benitops <benitoparsan@gmail.com>
2022-02-04 11:03:22 +0100

    HTML y estilo CSS básico

--- mis_estudios/index.php
+++ mis_estudios/index.php
@@ -3,3 +3,19 @@
 include_once('app/Configuracion.php');
 
 ?>
+
+<html lang="es">
+    <head>
+        <title>Asignaturas - Mis Estudios</title>
+        <link rel="stylesheet" href="estilos.css">
+
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+        </style>
+    </head>
+    <body>
+        <div>
+            <h1 class="centrado">ASIGNATURAS</h1>
+        </div>
+    </body>
+</html>
