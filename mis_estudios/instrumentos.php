03c06ea751a0699e2d4b47a0f0793215e7ec9825
benitops <benitoparsan@gmail.com>
2022-02-17 17:15:36 +0100

    Arreglo pequeño

--- mis_estudios/instrumentos.php
+++ mis_estudios/instrumentos.php
@@ -26,7 +26,7 @@ if(isset($_POST['instrumentos'])){
         $ins->setNombre($i['nombre']);
         $ins->setPeso($i['peso']);
         $ins->setCalificacion($i['calificacion']);
-        if(isset($ins['clave'])){
+        if(isset($i['clave'])){
             $ins->setClave($i['clave']);
             if(!$ins->actualizar()){
                 echo "Error al actualizar el instrumento";
