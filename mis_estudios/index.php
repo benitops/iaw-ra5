2c4c151f7fe878fb989a8c0e939ec0c5b3f50dc4
benitops <benitoparsan@gmail.com>
2022-02-18 20:17:36 +0100

    Cambios menores

--- mis_estudios/index.php
+++ mis_estudios/index.php
@@ -34,7 +34,6 @@ if(isset($_GET['asignatura']) && $_GET['operacion'] == 'eliminar'){
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Asignaturas - Mis Estudios</title>
 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
         <style>
             @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
         </style>
